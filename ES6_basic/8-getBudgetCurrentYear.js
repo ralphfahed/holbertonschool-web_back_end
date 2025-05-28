268d085796b98518551f0b1483bef8f991a7c1bf
ralph <ralphh.fahed@gmail.com>
2025-05-28 15:48:43 +0000

    8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,10 +4,13 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
   const budget = {
-  [`income-${getCurrentYear()}`] : income;
-  [`gdp-${getCurrentYear()}`] : gdp;
-  [`capita-${getCurrentYear()}`] : capita;
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
   };
+
   return budget;
 }
+
