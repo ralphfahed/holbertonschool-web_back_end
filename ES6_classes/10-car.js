107b251111b10a0b57dd1ec99bbddbf91016645b
ralph <ralphh.fahed@gmail.com>
2025-05-29 09:53:12 +0000

    10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Clone method
+  cloneCar() {
+    const Constructor = this.constructor;  // ✅ Now Constructor is defined
+    return new Constructor(this._brand, this._motor, this._color);
+  }
+}
