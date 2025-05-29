ad49aa0c2f1634c8d5bc21d9bcf478ad508e8380
ralph <ralphh.fahed@gmail.com>
2025-05-29 09:29:36 +0000

    6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call parent constructor with sqft
+
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
