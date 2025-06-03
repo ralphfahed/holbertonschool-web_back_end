331cf8f29145f78758042f52ab113cf9f55bf1ee
ralph <ralphh.fahed@gmail.com>
2025-06-03 08:48:04 +0000

    5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,11 +4,10 @@ function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(buffer);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
 
-  int8[position] = value;
-
-  return buffer;
+  return view;
 }
 
 export default createInt8TypedArray;
