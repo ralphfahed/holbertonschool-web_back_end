503799834e5efb0741e4a5de01c9e662103e30e3
ralph <ralphh.fahed@gmail.com>
2025-06-03 07:53:40 +0000

    1

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+function getListStudentIds(students) {
+  if (Array.isArray(students)) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
+
+export default getListStudentIds;
