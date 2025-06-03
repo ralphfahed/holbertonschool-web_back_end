40b519b7f147e0ea8da3e3fdf8f4fbf5035c3cc4
ralph <ralphh.fahed@gmail.com>
2025-06-03 09:30:39 +0000

    8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || !startString) {
+    return '';
+  }
   const result = [];
   for (const value of set) {
     if (value.startsWith(startString)) {
