8f7311f4876d66d851a02c1edb631f5f1e6b8751
ralph <ralphh.fahed@gmail.com>
2025-06-03 09:35:48 +0000

    9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+function groceriesList() {
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Milk', 1);
+  groceries.set('Cheese', 1);
+  return groceries;
+}
+
+export default groceriesList;
