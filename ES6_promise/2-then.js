726329e774f1a5f0a0dfa0aa9e74e05b57020f80
ralph <ralphh.fahed@gmail.com>
2025-06-03 10:34:41 +0000

    2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 function handleResponseFromAPI(promise) {
   return promise
-    .then(() => (console.log('Got a response from the API'), { status: 200, body: 'success' }))
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
     .catch(() => new Error());
 }
 
