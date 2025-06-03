67e694faa9e5bb8b5d90767cb5085cd0291f364d
ralph <ralphh.fahed@gmail.com>
2025-06-03 08:06:23 +0000

    2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-function getStudentsByLocation (students , city) {
-	return students.filter((student) => student.location === city);
+function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
 }
 export default getStudentsByLocation;
