cfb99ff745c9d5054fedf9cb41f87e680ce21763
ralph <ralphh.fahed@gmail.com>
2025-07-14 09:17:44 +0000

    4-5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+const databasePath = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(databasePath)
+      .then((output) => {
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch(() => {
+        res.statusCode = 500;
+        res.end('Cannot load the database');
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
